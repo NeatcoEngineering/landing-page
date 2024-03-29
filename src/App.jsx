57917d9df0a8cb97c57a7b003bebd9225d4fc3d9
Neatco Engineering <neatco@Kians-MacBook-Pro-2.local>
2024-03-29 17:43:45 -0400

    adding intelview cloud to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe, faChartBar } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faYoutube, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import logo from './assets/Logo.png';
 import banner from './assets/banner.png'
@@ -19,23 +19,26 @@ const App = () => {
         <div className="flex items-center mb-6">
           <img src={logo} alt="Logo" className="w-16 h-16 mr-4" />
           <div>
-            <h1 className="text-2xl font-bold text-white">Neatco Engineering</h1>
+            <h1 className="text-2xl font-bold text-white text-3xl">Neatco Engineering</h1>
             <p className="text-md text-center text-gray-300">AI for the Circular Economy</p>
           </div>
         </div>
 
         {/* Links Column */}
         <div className="space-y-4">
-          <a href="https://www.neatcoeng.com" className="flex border-b items-center text-white text-xl">
+        <a href="https://intelview-cloud.web.app" className="flex border-b items-center text-white text-3xl hover:text-gray-300">
+            <FontAwesomeIcon icon={faChartBar} className="mr-2" /> IntelView
+          </a>
+          <a href="https://www.neatcoeng.com" className="flex border-b items-center text-white text-3xl hover:text-gray-300">
             <FontAwesomeIcon icon={faGlobe} className="mr-2" /> Website
           </a>
-          <a href="https://www.linkedin.com/company/42872230/" className="flex border-b items-center text-white text-xl">
+          <a href="https://www.linkedin.com/company/42872230/" className="flex border-b items-center text-white text-3xl hover:text-gray-300">
             <FontAwesomeIcon icon={faLinkedin} className="mr-2 text-blue-700" /> LinkedIn
           </a>
-          <a href="https://www.youtube.com/channel/UCeVTUXQMhMoxSp2MsiKuJsg" className="flex border-b items-center text-white text-xl">
+          <a href="https://www.youtube.com/channel/UCeVTUXQMhMoxSp2MsiKuJsg" className="flex border-b items-center text-white text-3xl hover:text-gray-300">
             <FontAwesomeIcon icon={faYoutube} className="mr-2 text-red-600" /> YouTube
           </a>
-          <a href="https://twitter.com/neatcoeng?lang=en" className="flex border-b items-center text-white text-xl">
+          <a href="https://twitter.com/neatcoeng?lang=en" className="flex border-b items-center text-white text-3xl hover:text-gray-300">
             <FontAwesomeIcon icon={faXTwitter} className="mr-2 text-black" /> X (Twitter)
           </a>
         </div>
